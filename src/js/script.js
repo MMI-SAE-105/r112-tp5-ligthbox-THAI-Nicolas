f6c11c3f7a3db1341c51a4da43160436a1ffda3a
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2024-12-03 23:03:29 +0100

    Ajout d'une classe "sortie" et "hidden" quand la Lightbox se ferme (j'ai du rajouter "hidden" en plus car la lightbox restait visible après fermeture

--- src/js/script.js
+++ src/js/script.js
@@ -4,23 +4,16 @@ const lightboxImg = document.querySelector("#lightbox > img");
 
 document.body.addEventListener("click", (evt) => {
     if (evt.target.matches("img[data-full-img]")) {
-        const fullImgUrl = evt.target.dataset.fullImg;
-        lightboxImg.src = fullImgUrl;
-        lightbox.style.display = "flex";
+        lightboxImg.src = evt.target.dataset.fullImg;
+        lightbox.classList.remove("hidden", "sortie");
         lightbox.showModal();
     }
-    lightbox.addEventListener("click", () =>
-        lightbox.close());
-    if (evt.target === lightboxImg) {
-        lightbox.close();
-        lightbox.style.display = "none";
-    }
-})
 
+})
 lightbox.addEventListener("click", (evt) => {
     lightbox.classList.add("sortie");
     lightbox.addEventListener("animationend", () => {
-        lightbox.classList.remove("sortie");
         lightbox.close();
+        lightbox.classList.add("hidden");
     }, { once: true });
-});
+})
