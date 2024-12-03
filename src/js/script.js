b8909fae004fbdfa899dbf80f16e72000faee300
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2024-12-03 17:21:42 +0100

    Modifications du JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,26 @@
 // *** LIGHTBOX ***
 const lightbox = document.querySelector("#lightbox");
 const lightboxImg = document.querySelector("#lightbox > img");
-const closeBtn = document.querySelector("#lightbox > button");
 
 document.body.addEventListener("click", (evt) => {
     if (evt.target.matches("img[data-full-img]")) {
         const fullImgUrl = evt.target.dataset.fullImg;
         lightboxImg.src = fullImgUrl;
+        lightbox.style.display = "flex";
         lightbox.showModal();
     }
-    closeBtn.addEventListener("click", () => lightbox.close());
-    if (evt.target === lightbox) {
+    lightbox.addEventListener("click", () =>
+        lightbox.close());
+    if (evt.target === lightboxImg) {
         lightbox.close();
+        lightbox.style.display = "none";
     }
-}
+})
 
-
-
-)
+lightbox.addEventListener("click", (evt) => {
+    lightbox.classList.add("sortie");
+    lightbox.addEventListener("animationend", () => {
+        lightbox.classList.remove("sortie");
+        lightbox.close();
+    }, { once: true });
+});
