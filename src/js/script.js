337fd1a345a5a85e268bcba5798d4fbb6317cf95
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2024-12-03 16:22:03 +0100

    Mise en place du JS et ajustements CSS

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 // *** LIGHTBOX ***
+const lightbox = document.querySelector("#lightbox");
+const lightboxImg = document.querySelector("#lightbox > img");
+const closeBtn = document.querySelector("#lightbox > button");
+
+document.body.addEventListener("click", (evt) => {
+    if (evt.target.matches("img[data-full-img]")) {
+        const fullImgUrl = evt.target.dataset.fullImg;
+        lightboxImg.src = fullImgUrl;
+        lightbox.showModal();
+    }
+    closeBtn.addEventListener("click", () => lightbox.close());
+}
+
+
+
+)
